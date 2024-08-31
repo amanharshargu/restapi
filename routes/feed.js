6f8be0d41f21242aef243acee9fd49498d0b4e6f
amanharshargu <aman.choubey@argusoft.in>
2024-08-31 14:26:12 +0530

    1.0

---
+++ routes/feed.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { body } = require('express-validator');
+
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+router.get('/posts', isAuth, feedController.getPosts);
+
+router.post(
+  '/post',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage('Title must be at least 5 characters long'),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage('Content must be at least 5 characters long'),
+  ],
+  feedController.createPost
+);
+
+router.get('/post/:postId', isAuth, feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  isAuth,
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage('Title must be at least 5 characters long'),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage('Content must be at least 5 characters long'),
+    body('imageUrl')
+      .optional()
+      .isURL()
+      .withMessage('Invalid image URL'),
+  ],
+  feedController.updatePost
+);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
+
+module.exports = router;
